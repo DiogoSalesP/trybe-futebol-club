1e100c1a39314728c24989032f573691fb481388
Diogo Sales <diogosales.ads@gmail.com>
2023-01-26 23:29:14 -0300

    req01

---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,41 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  role: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  email: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(255),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default User;
