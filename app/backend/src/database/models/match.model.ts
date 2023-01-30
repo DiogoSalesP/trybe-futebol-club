35a57d020ba856f7a579e56f454e39ca891db20a
Diogo Sales <diogosales.ads@gmail.com>
2023-01-30 13:19:59 -0300

    feat: req14 and req18

---
+++ app/backend/src/database/models/match.model.ts
@@ -0,0 +1,54 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+import Teams from './team.model';
+
+class Matches extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeamId' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeamId' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeamId', as: 'homeMatchesId' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeamId', as: 'awayMatchesId' });
+
+export default Matches;
---
+++ app/backend/src/database/models/team.model.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(255),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
